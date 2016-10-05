c41713de949e13cccabe9dcb08012438c5ab923a
StephaniePC <teff.padilla15@gmail.com>
2016-10-05 00:50:41 -0600

    Mi segundo commit

---
+++ Clase1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaInvertida
+{
+    class Clase1
+    {
+        List<int> l1 = new List<int>();
+        public int agregar(int numero)
+        {
+
+            l1.Add(numero);
+
+            return numero;
+        }
+        public void imprimir()
+        {
+            Console.WriteLine("--------Lista original--------");
+            for (int i = 0; i < l1.Count; i++)
+            {
+                    Console.WriteLine(l1[i]);
+              
+            }
+            Console.WriteLine("------------------------------");
+        }
+        public void imprimirInverso()
+        {
+            Console.WriteLine("------- Lista invertida------");
+            for (int i = l1.Count-1; i >=0; i--)
+            {
+                
+                Console.WriteLine(l1[i]);
+                
+            }
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
